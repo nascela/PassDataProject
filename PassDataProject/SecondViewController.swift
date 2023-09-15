1cba76fcb8b9cf83af945f93c83b2e7115d4a853
nascela <serjono5522@gmail.com>
2023-09-15 11:44:00 +0200

    Initial Commit

---
+++ PassDataProject/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  PassDataProject
+//
+//  Created by Сергей Александров on 15.09.2023.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBAction func goBackTapped(_ sender: UIButton) {
+    }
+    
+
+}
---
+++ PassDataProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassDataProject
+//
+//  Created by Сергей Александров on 15.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loginTF: UITextField!
+    @IBOutlet weak var paswordTF: UITextField!
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    
+}
+
