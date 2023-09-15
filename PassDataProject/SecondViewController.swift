5acef52503bb1b03e2de11a588343e041252a9aa
nascela <serjono5522@gmail.com>
2023-09-15 12:15:38 +0200

    add functional

--- PassDataProject/SecondViewController.swift
+++ PassDataProject/SecondViewController.swift
@@ -9,7 +9,16 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    var login: String?
+    
     @IBOutlet weak var label: UILabel!
+    // функция при загрузке контроллера
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let login = self.login else {return}
+        label.text = "Hello, \(login)"
+    }
     @IBAction func goBackTapped(_ sender: UIButton) {
     }
     
--- PassDataProject/ViewController.swift
+++ PassDataProject/ViewController.swift
@@ -12,7 +12,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var loginTF: UITextField!
     @IBOutlet weak var paswordTF: UITextField!
     
+    // переход на 2-ой контроллер при нажатии на кнопку
     @IBAction func loginTapped(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "detailSegue", sender: nil)
+    }
+    
+    // передача в 2-ой контроллер текста из loginTF
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // ссыллка на контроллер
+        guard let dvc = segue.destination as? SecondViewController else {return}
+        // передаем по ссылке занчение из loginTF в переменную login находящуюся во 2-ом контроллере
+        dvc.login = loginTF.text
+    }
+    
+    
+    //закрытие клавиатуры по нажатию на дисплей
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
 }
